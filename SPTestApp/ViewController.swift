2bc9d17842c77f146398f011220dffca8bea92a1
Gerald <geraldk675@gmail.com>
2021-03-10 21:21:02 +1100

    Make working

--- SPTestApp/ViewController.swift
+++ SPTestApp/ViewController.swift
@@ -6,68 +6,63 @@
 //
 
 import UIKit
+import SPUIElements
 
 class ViewController: UIViewController {
-//    private lazy var dataSource = makeDataSource()
+    private lazy var dataSource = makeDataSource()
     private let cellReuseIdentifier = "cell"
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let elements = UIElements()
+        //------------
+        tableView.register(elements.tableCellNib, forCellReuseIdentifier: cellReuseIdentifier)
+        //------------
+        tableView.dataSource = dataSource
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) {
+            self.update(with: [
+                CellViewModel(title: "hello1", subtitle: "I am a cool subtitle"),
+                CellViewModel(title: "hello2", subtitle: "I am a cooler subtitle"),
+                CellViewModel(title: "hello3", subtitle: "I am the coolest subtitle"),
+            ])
+        }
     }
 }
 
+enum Section: CaseIterable {
+    case first
+}
 
+struct CellViewModel: Hashable {
+    var title: String
+    var subtitle: String
+}
 
+private extension ViewController {
+    func makeDataSource() -> UITableViewDiffableDataSource<Section, CellViewModel> {
+        let reuseIdentifier = cellReuseIdentifier
 
+        return UITableViewDiffableDataSource(
+            tableView: tableView,
+            cellProvider: {  tableView, indexPath, viewModel in
+                guard let cell = tableView.dequeueReusableCell(
+                    withIdentifier: reuseIdentifier,
+                    for: indexPath
+                ) as? TableCell else { return nil }
 
-//enum Section: CaseIterable {
-//    case first
-//}
-//
-//struct CellViewModel: Hashable {
-//    var title: String
-//    var subtitle: String
-//}
-//
-//private extension ViewController {
-//    func makeDataSource() -> UITableViewDiffableDataSource<Section, CellViewModel> {
-//        let reuseIdentifier = cellReuseIdentifier
-//
-//        return UITableViewDiffableDataSource(
-//            tableView: tableView,
-//            cellProvider: {  tableView, indexPath, viewModel in
-//                guard let cell = tableView.dequeueReusableCell(
-//                    withIdentifier: reuseIdentifier,
-//                    for: indexPath
-//                ) as? TableCell else { return nil }
-//
-//                cell.titleLabel.text = viewModel.title
-//                cell.subtitleLabel.text = viewModel.subtitle
-//                return cell
-//            }
-//        )
-//    }
-//
-//    func update(with models: [CellViewModel]) {
-//        var snapshot = NSDiffableDataSourceSnapshot<Section, CellViewModel>()
-//        snapshot.appendSections(Section.allCases)
-//        snapshot.appendItems(models, toSection: .first)
-//        dataSource.apply(snapshot, animatingDifferences: true)
-//       }
-//}
+                cell.titleLabel.text = viewModel.title
+                cell.subtitleLabel.text = viewModel.subtitle
+                return cell
+            }
+        )
+    }
 
-
-/*
- let elements = UIElements()
- tableView.register(elements.tableCellNib, forCellReuseIdentifier: cellReuseIdentifier)
- tableView.dataSource = dataSource
- 
- DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) {
-     self.update(with: [
-         CellViewModel(title: "hello1", subtitle: "I am a cool subtitle"),
-         CellViewModel(title: "hello2", subtitle: "I am a cooler subtitle"),
-         CellViewModel(title: "hello3", subtitle: "I am the coolest subtitle"),
-     ])
- }
- */
+    func update(with models: [CellViewModel]) {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, CellViewModel>()
+        snapshot.appendSections(Section.allCases)
+        snapshot.appendItems(models, toSection: .first)
+        dataSource.apply(snapshot, animatingDifferences: true)
+       }
+}
